a22d149f04537265e4bc867a1b5bf7b52ca49a9d
acmac <craig.mclellen@eighty20solutions.com.au>
2024-02-05 12:35:36 +0800

    Fix

--- functions/PiiDocIdentify.Functions/PiiDocIdentifyFunction.cs
+++ functions/PiiDocIdentify.Functions/PiiDocIdentifyFunction.cs
@@ -15,100 +15,110 @@ namespace PiiDocIdentify.Functions
         public PiiDocIdentifyFunction(
             ILogger<PiiDocIdentifyFunction> logger,
             DocumentAnalysisClient documentAnalysisClient
-            )
+        )
         {
             _logger = logger;
             _documentAnalysisClient = documentAnalysisClient;
         }
 
         [Function(nameof(PiiDocIdentifyFunction))]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req, CancellationToken cancellationToken)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            CancellationToken cancellationToken)
         {
-            Uri idDocumentUri = new Uri("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/rest-api/identity_documents.png");
+            var content = await new StreamReader(req.Body).ReadToEndAsync();
+            _logger.LogInformation("Content is {Content}", content);
 
-            AnalyzeDocumentOperation operation = await _documentAnalysisClient.AnalyzeDocumentFromUriAsync(WaitUntil.Completed, "prebuilt-idDocument", idDocumentUri, cancellationToken: cancellationToken);
+            var idDocumentUri =
+                new Uri(
+                    "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/rest-api/identity_documents.png");
 
-            AnalyzeResult identityDocuments = operation.Value;
+            var operation = await _documentAnalysisClient.AnalyzeDocumentFromUriAsync(WaitUntil.Completed,
+                "prebuilt-idDocument", idDocumentUri, cancellationToken: cancellationToken);
 
-            AnalyzedDocument identityDocument = identityDocuments.Documents.Single();
+            var identityDocuments = operation.Value;
 
-            if (identityDocument.Fields.TryGetValue("Address", out DocumentField addressField))
+            var identityDocument = identityDocuments.Documents.Single();
+
+            if (identityDocument.Fields.TryGetValue("Address", out var addressField))
             {
                 if (addressField.FieldType == DocumentFieldType.String)
                 {
-                    string address = addressField.Value.AsString();
+                    var address = addressField.Value.AsString();
                     Console.WriteLine($"Address: '{address}', with confidence {addressField.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("CountryRegion", out DocumentField countryRegionField))
+            if (identityDocument.Fields.TryGetValue("CountryRegion", out var countryRegionField))
             {
                 if (countryRegionField.FieldType == DocumentFieldType.CountryRegion)
                 {
-                    string countryRegion = countryRegionField.Value.AsCountryRegion();
-                    Console.WriteLine($"CountryRegion: '{countryRegion}', with confidence {countryRegionField.Confidence}");
+                    var countryRegion = countryRegionField.Value.AsCountryRegion();
+                    Console.WriteLine(
+                        $"CountryRegion: '{countryRegion}', with confidence {countryRegionField.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("DateOfBirth", out DocumentField dateOfBirthField))
+            if (identityDocument.Fields.TryGetValue("DateOfBirth", out var dateOfBirthField))
             {
                 if (dateOfBirthField.FieldType == DocumentFieldType.Date)
                 {
-                    DateTimeOffset dateOfBirth = dateOfBirthField.Value.AsDate();
+                    var dateOfBirth = dateOfBirthField.Value.AsDate();
                     Console.WriteLine($"Date Of Birth: '{dateOfBirth}', with confidence {dateOfBirthField.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("DateOfExpiration", out DocumentField dateOfExpirationField))
+            if (identityDocument.Fields.TryGetValue("DateOfExpiration", out var dateOfExpirationField))
             {
                 if (dateOfExpirationField.FieldType == DocumentFieldType.Date)
                 {
-                    DateTimeOffset dateOfExpiration = dateOfExpirationField.Value.AsDate();
-                    Console.WriteLine($"Date Of Expiration: '{dateOfExpiration}', with confidence {dateOfExpirationField.Confidence}");
+                    var dateOfExpiration = dateOfExpirationField.Value.AsDate();
+                    Console.WriteLine(
+                        $"Date Of Expiration: '{dateOfExpiration}', with confidence {dateOfExpirationField.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("DocumentNumber", out DocumentField documentNumberField))
+            if (identityDocument.Fields.TryGetValue("DocumentNumber", out var documentNumberField))
             {
                 if (documentNumberField.FieldType == DocumentFieldType.String)
                 {
-                    string documentNumber = documentNumberField.Value.AsString();
-                    Console.WriteLine($"Document Number: '{documentNumber}', with confidence {documentNumberField.Confidence}");
+                    var documentNumber = documentNumberField.Value.AsString();
+                    Console.WriteLine(
+                        $"Document Number: '{documentNumber}', with confidence {documentNumberField.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("FirstName", out DocumentField firstNameField))
+            if (identityDocument.Fields.TryGetValue("FirstName", out var firstNameField))
             {
                 if (firstNameField.FieldType == DocumentFieldType.String)
                 {
-                    string firstName = firstNameField.Value.AsString();
+                    var firstName = firstNameField.Value.AsString();
                     Console.WriteLine($"First Name: '{firstName}', with confidence {firstNameField.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("LastName", out DocumentField lastNameField))
+            if (identityDocument.Fields.TryGetValue("LastName", out var lastNameField))
             {
                 if (lastNameField.FieldType == DocumentFieldType.String)
                 {
-                    string lastName = lastNameField.Value.AsString();
+                    var lastName = lastNameField.Value.AsString();
                     Console.WriteLine($"Last Name: '{lastName}', with confidence {lastNameField.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("Region", out DocumentField regionfield))
+            if (identityDocument.Fields.TryGetValue("Region", out var regionfield))
             {
                 if (regionfield.FieldType == DocumentFieldType.String)
                 {
-                    string region = regionfield.Value.AsString();
+                    var region = regionfield.Value.AsString();
                     Console.WriteLine($"Region: '{region}', with confidence {regionfield.Confidence}");
                 }
             }
 
-            if (identityDocument.Fields.TryGetValue("Sex", out DocumentField sexfield))
+            if (identityDocument.Fields.TryGetValue("Sex", out var sexfield))
             {
                 if (sexfield.FieldType == DocumentFieldType.String)
                 {
-                    string sex = sexfield.Value.AsString();
+                    var sex = sexfield.Value.AsString();
                     Console.WriteLine($"Sex: '{sex}', with confidence {sexfield.Confidence}");
                 }
             }
