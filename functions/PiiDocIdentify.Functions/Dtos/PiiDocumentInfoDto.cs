a64258ab67be29500ee0c01aea5c83c49a9b2e29
Craig McLellen <cmclellen@gmail.com>
2024-02-06 15:51:13 +0800

    Create pipeline to create index, indexer, skillset & datasource within AI Search (#3)

---
+++ functions/PiiDocIdentify.Functions/Dtos/PiiDocumentInfoDto.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+
+namespace PiiDocIdentify.Functions.Dtos
+{
+    public class PiiDocumentInfoDto
+    {
+        public string DocumentType { get; set; }
+        public string Address { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public DateTimeOffset DateOfExpiration { get; set; }
+        public string DocumentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+    }
+}
